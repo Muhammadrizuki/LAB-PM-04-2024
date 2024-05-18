3b00d00d86d5c3f92950645ffea3683fb45dde8f
Ardika13 <ardikaardika586@gmail.com>
2024-05-18 21:47:56 +0700

    Kumpul Tugas

---
+++ H071221002/Pertemuan-3/TugasPraktikum3/app/src/main/java/com/example/tugaspraktikum3/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.tugaspraktikum3;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+}
